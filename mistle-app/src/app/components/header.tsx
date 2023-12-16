048406f5d07e3dc2599bcbe585d4426615d7f9d6
aibiansari <aibiansari1@gmail.com>
2023-12-16 17:53:32 +0500

    Code Updated

--- mistle-app/src/app/components/header.tsx
+++ mistle-app/src/app/components/header.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 //Import LOGO
 
 function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 flex justify-between items-center bg-darkbg text-white p-4 bg-opacity-80">
       <div className="flex-wrap">
-        <p >Logo</p>
+        <p>Logo</p>
       </div>
       <div className="flex gap-4">
-        <a href="#" className="hover:text-gray-300">About Us</a>
-        <a href="#" className="hover:text-gray-300">Sign Up</a>
-        <a href="#" className="hover:text-gray-300">Sign In</a>
+        <a href="#" className="hover:text-gray-300">
+          About Us
+        </a>
+        <a href="#" className="hover:text-gray-300">
+          Sign Up
+        </a>
+        <a href="#" className="hover:text-gray-300">
+          Sign In
+        </a>
       </div>
     </header>
   );
--- mistle-app/src/app/mistle/page.tsx
+++ mistle-app/src/app/mistle/page.tsx
@@ -1,98 +1,265 @@
-"use client"
+"use client";
+import * as go from "gojs";
+import { produce } from "immer";
+import * as React from "react";
 
-import React from 'react';
-
-import * as go from 'gojs';
-import { ReactDiagram } from 'gojs-react';
-
-import './page.css';  // contains .diagram-component CSS
-
-// ...
+import { DiagramWrapper } from "./DiagramWrapper";
 
 /**
- * Diagram initialization method, which is passed to the ReactDiagram component.
- * This method is responsible for making the diagram and initializing the model and any templates.
- * The model's data should not be set here, as the ReactDiagram component handles that via the other props.
+ * Use a linkDataArray since we'll be using a GraphLinksModel,
+ * and modelData for demonstration purposes. Note, though, that
+ * both are optional props in ReactDiagram.
  */
-function initDiagram() {
-  const $ = go.GraphObject.make;
-  // set your license key here before creating the diagram: go.Diagram.licenseKey = "...";
-  const diagram =
-    $(go.Diagram,
-      {
-        'undoManager.isEnabled': true,  // must be set to allow for model change listening
-        // 'undoManager.maxHistoryLength': 0,  // uncomment disable undo/redo functionality
-        'clickCreatingTool.archetypeNodeData': { text: 'new node', color: 'lightblue' },
-        model: new go.GraphLinksModel(
-          {
-            linkKeyProperty: 'key'  // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
-          })
-      });
 
-  // define a simple Node template
-  diagram.nodeTemplate =
-    $(go.Node, 'Auto',  // the Shape will go around the TextBlock
-      new go.Binding('location', 'loc', go.Point.parse).makeTwoWay(go.Point.stringify),
-      $(go.Shape, 'RoundedRectangle',
-        { name: 'SHAPE', fill: 'white', strokeWidth: 1 },
-        // Shape.fill is bound to Node.data.color
-        new go.Binding('fill', 'color')),
-      $(go.TextBlock,
-        { margin: 8, editable: true },  // some room around the text
-        new go.Binding('text').makeTwoWay()
-      )
-    );
-    // define the link template
-    diagram.linkTemplate =
-    $(go.Link,
-      {
-        routing: go.Link.Normal,
-        curve: go.Link.Bezier,
-        corner: 10,
-        toShortLength: 4,
-        fromShortLength: 4,
-        fromEndSegmentLength: 20,
-        toEndSegmentLength: 20,
-        layerName: 'Background',
-        visible: true
+interface AppState {
+  nodeDataArray: Array<go.ObjectData>;
+  linkDataArray: Array<go.ObjectData>;
+  modelData: go.ObjectData;
+  selectedData: go.ObjectData | null;
+  skipsDiagramUpdate: boolean;
+}
+
+class App extends React.Component<{}, AppState> {
+  // Maps to store key -> arr index for quick lookups
+  private mapNodeKeyIdx: Map<go.Key, number>;
+  private mapLinkKeyIdx: Map<go.Key, number>;
+
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      nodeDataArray: [
+        { key: 0, text: "Alpha", color: "lightblue", loc: "0 0" },
+        { key: 1, text: "Beta", color: "orange", loc: "100 0" },
+      ],
+      linkDataArray: [{ key: -1, from: 0, to: 1 }],
+      modelData: {
+        canRelink: true,
       },
-      $(go.Shape,
-        { strokeWidth: 2}, // Add toArrow property for arrowhead at the to end
-        new go.Binding('stroke', 'color'))
+      selectedData: null,
+      skipsDiagramUpdate: false,
+    };
+
+    // init maps
+    this.mapNodeKeyIdx = new Map<go.Key, number>();
+    this.mapLinkKeyIdx = new Map<go.Key, number>();
+    this.refreshNodeIndex(this.state.nodeDataArray);
+    this.refreshLinkIndex(this.state.linkDataArray);
+
+    // bind handler methods
+    this.handleDiagramEvent = this.handleDiagramEvent.bind(this);
+    this.handleModelChange = this.handleModelChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  /**
+   * Update map of node keys to their index in the array.
+   */
+
+  private refreshNodeIndex(nodeArr: Array<go.ObjectData>) {
+    this.mapNodeKeyIdx.clear();
+    nodeArr.forEach((n: go.ObjectData, idx: number) => {
+      this.mapNodeKeyIdx.set(n.key, idx);
+    });
+  }
+
+  /**
+   * Update map of link keys to their index in the array.
+   */
+
+  private refreshLinkIndex(linkArr: Array<go.ObjectData>) {
+    this.mapLinkKeyIdx.clear();
+    linkArr.forEach((l: go.ObjectData, idx: number) => {
+      this.mapLinkKeyIdx.set(l.key, idx);
+    });
+  }
+
+  /**
+   * Handle any relevant DiagramEvents, in this case just selection changes.
+   * On ChangedSelection, find the corresponding data and set the selectedData state.
+   * @param e a GoJS DiagramEvent
+   */
+
+  public handleDiagramEvent(e: go.DiagramEvent) {
+    const name = e.name;
+    switch (name) {
+      case "ChangedSelection": {
+        const sel = e.subject.first();
+        this.setState(
+          produce((draft: AppState) => {
+            if (sel) {
+              if (sel instanceof go.Node) {
+                const idx = this.mapNodeKeyIdx.get(sel.key);
+                if (idx !== undefined && idx >= 0) {
+                  const nd = draft.nodeDataArray[idx];
+                  draft.selectedData = nd;
+                }
+              } else if (sel instanceof go.Link) {
+                const idx = this.mapLinkKeyIdx.get(sel.key);
+                if (idx !== undefined && idx >= 0) {
+                  const ld = draft.linkDataArray[idx];
+                  draft.selectedData = ld;
+                }
+              }
+            } else {
+              draft.selectedData = null;
+            }
+          })
+        );
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+  /**
+   * Handle GoJS model changes, which output an object of data changes via Model.toIncrementalData.
+   * This method iterates over those changes and updates state to keep in sync with the GoJS model.
+   * @param obj a JSON-formatted string
+   */
+
+  public handleModelChange(obj: go.IncrementalData) {
+    const insertedNodeKeys = obj.insertedNodeKeys;
+    const modifiedNodeData = obj.modifiedNodeData;
+    const removedNodeKeys = obj.removedNodeKeys;
+    const insertedLinkKeys = obj.insertedLinkKeys;
+    const modifiedLinkData = obj.modifiedLinkData;
+    const removedLinkKeys = obj.removedLinkKeys;
+    const modifiedModelData = obj.modelData;
+
+    // maintain maps of modified data so insertions don't need slow lookups
+    const modifiedNodeMap = new Map<go.Key, go.ObjectData>();
+    const modifiedLinkMap = new Map<go.Key, go.ObjectData>();
+    this.setState(
+      produce((draft: AppState) => {
+        let narr = draft.nodeDataArray;
+        if (modifiedNodeData) {
+          modifiedNodeData.forEach((nd: go.ObjectData) => {
+            modifiedNodeMap.set(nd.key, nd);
+            const idx = this.mapNodeKeyIdx.get(nd.key);
+            if (idx !== undefined && idx >= 0) {
+              narr[idx] = nd;
+              if (draft.selectedData && draft.selectedData.key === nd.key) {
+                draft.selectedData = nd;
+              }
+            }
+          });
+        }
+        if (insertedNodeKeys) {
+          insertedNodeKeys.forEach((key: go.Key) => {
+            const nd = modifiedNodeMap.get(key);
+            const idx = this.mapNodeKeyIdx.get(key);
+            if (nd && idx === undefined) {
+              // nodes won't be added if they already exist
+              this.mapNodeKeyIdx.set(nd.key, narr.length);
+              narr.push(nd);
+            }
+          });
+        }
+        if (removedNodeKeys) {
+          narr = narr.filter((nd: go.ObjectData) => {
+            if (removedNodeKeys.includes(nd.key)) {
+              return false;
+            }
+            return true;
+          });
+          draft.nodeDataArray = narr;
+          this.refreshNodeIndex(narr);
+        }
+
+        let larr = draft.linkDataArray;
+        if (modifiedLinkData) {
+          modifiedLinkData.forEach((ld: go.ObjectData) => {
+            modifiedLinkMap.set(ld.key, ld);
+            const idx = this.mapLinkKeyIdx.get(ld.key);
+            if (idx !== undefined && idx >= 0) {
+              larr[idx] = ld;
+              if (draft.selectedData && draft.selectedData.key === ld.key) {
+                draft.selectedData = ld;
+              }
+            }
+          });
+        }
+        if (insertedLinkKeys) {
+          insertedLinkKeys.forEach((key: go.Key) => {
+            const ld = modifiedLinkMap.get(key);
+            const idx = this.mapLinkKeyIdx.get(key);
+            if (ld && idx === undefined) {
+              // links won't be added if they already exist
+              this.mapLinkKeyIdx.set(ld.key, larr.length);
+              larr.push(ld);
+            }
+          });
+        }
+        if (removedLinkKeys) {
+          larr = larr.filter((ld: go.ObjectData) => {
+            if (removedLinkKeys.includes(ld.key)) {
+              return false;
+            }
+            return true;
+          });
+          draft.linkDataArray = larr;
+          this.refreshLinkIndex(larr);
+        }
+        // handle model data changes, for now just replacing with the supplied object
+        if (modifiedModelData) {
+          draft.modelData = modifiedModelData;
+        }
+        draft.skipsDiagramUpdate = true; // the GoJS model already knows about these updates
+      })
     );
-  return diagram;
+  }
+
+  /**
+   * Handle inspector changes, and on input field blurs, update node/link data state.
+   * @param path the path to the property being modified
+   * @param value the new value of that property
+   * @param isBlur whether the input event was a blur, indicating the edit is complete
+   */
+  public handleInputChange(path: string, value: string, isBlur: boolean) {
+    this.setState(
+      produce((draft: AppState) => {
+        const data = draft.selectedData as go.ObjectData; // only reached if selectedData isn't null
+        data[path] = value;
+        if (isBlur) {
+          const key = data.key;
+          if (key < 0) {
+            // negative keys are links
+            const idx = this.mapLinkKeyIdx.get(key);
+            if (idx !== undefined && idx >= 0) {
+              draft.linkDataArray[idx] = data;
+              draft.skipsDiagramUpdate = false;
+            }
+          } else {
+            const idx = this.mapNodeKeyIdx.get(key);
+            if (idx !== undefined && idx >= 0) {
+              draft.nodeDataArray[idx] = data;
+              draft.skipsDiagramUpdate = false;
+            }
+          }
+        }
+      })
+    );
+  }
+
+  /**
+   * Handle changes to the checkbox on whether to allow relinking.
+   * @param e a change event from the checkbox
+   */
+  public render() {
+    return (
+      <div>
+        <DiagramWrapper
+          nodeDataArray={this.state.nodeDataArray}
+          linkDataArray={this.state.linkDataArray}
+          modelData={this.state.modelData}
+          skipsDiagramUpdate={this.state.skipsDiagramUpdate}
+          onDiagramEvent={this.handleDiagramEvent}
+          onModelChange={this.handleModelChange}
+        />
+      </div>
+    );
+  }
 }
 
-/**
- * This function handles any changes to the GoJS model.
- * It is here that you would make any updates to your React state, which is discussed below.
- */
-function handleModelChange(changes: any) {
-  console.log("CHANGED")
-}
-
-// render function...
-export default function App() {
-  return (
-    <div>
-      <ReactDiagram
-        initDiagram={initDiagram}
-        divClassName='diagram-component'
-        nodeDataArray={[
-          { key: 0, text: 'Alpha', color: 'lightblue', loc: '0 0' },
-          { key: 1, text: 'Beta', color: 'orange', loc: '150 0' },
-          { key: 2, text: 'Gamma', color: 'lightgreen', loc: '0 150' },
-          { key: 3, text: 'Delta', color: 'pink', loc: '150 150' }
-        ]}
-        linkDataArray={[
-          { key: -1, from: 0, to: 1, color: 'white'},
-          { key: -2, from: 0, to: 2, color: 'white' },
-          { key: -3, from: 1, to: 1, color: 'white' },
-          { key: -4, from: 2, to: 3, color: 'white' },
-          { key: -5, from: 3, to: 0, color: 'white' }
-        ]}
-        onModelChange={handleModelChange}
-      />
-    </div>
-  );
-}
+export default App;
