4b95d174930ecaab03e116e210512355a41ab4f9
iKamranShahzad <mailkamranshahzad@gmail.com>
2024-04-21 15:12:52 +0500

    Removed Deprecated Option

--- mistle-app/src/app/utils/db.js
+++ mistle-app/src/app/utils/db.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose";
 const connect = async () => {
   if (mongoose.connections[0].readyState) return;
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL, {});
     console.log("MongoDB Connected");
   } catch (error) {
     throw new Error("Error connecting to mongoose");
